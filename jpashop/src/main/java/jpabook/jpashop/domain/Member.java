14a5f2a371021fe4d2a2d3a5e806c91406c605e7
kha-github <rlahyuna1234@gmail.com>
2021-09-03 17:02:59 +0900

    실전 예제2 - 연관관계 매핑 시작

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -13,6 +15,12 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+    //null point exception 방지 등을 위해 일단 객체 할당
+    //one의 입장이므로 연관관계의 주인은 다에게 있음
+    //양방향이 필요한 상황이라면 설계에서 관계를 잘 끊어내지 못했는지를 생각해보기.
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
